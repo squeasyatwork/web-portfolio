6d35b1b33182d78fac8d1fb30407f265f54b1217
squeasyatwork <amal0055@student.monash.edu>
2024-03-20 17:28:17 +1100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -15,3 +15,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let arr = [1, 2, 3, 4, 5]
