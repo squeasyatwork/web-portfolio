bd339470c822d81fe56f30839cc8b34b2a583c49
squeasyatwork <amal0055@student.monash.edu>
2024-04-04 02:18:10 +1100

    nothing.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       {!isOpen && (
         <button
           onClick={toggleMenu}
-          className="fade-in hamburger w-full rounded-xl border-2 border-white shadow-md hover:shadow-gray-200 px-1.5 pb-1 -mt-1 h-full text-center bg-white hover:text-black hover:border-2 hover:border-rose-200 hover:shadow-md hover:shadow-rose-100 transition ease-in duration:400 hover:ease-in"
+          className="fade-in hamburger w-full rounded-xl border-2 border-white shadow-md hover:shadow-gray-200 px-1.5 pb-1 -mt-1 h-full text-center bg-white hover:text-black hover:border-2 hover:border-rose-400 hover:shadow-md hover:shadow-rose-100 transition ease-in duration:400 hover:ease-in"
         >
           ☰
         </button>
@@ -24,13 +24,13 @@ const Navbar = () => {
         <div className="fade-in text-3xl lg:text-4xl shadow-md rounded-xl menu w-full flex bg-[#FFFFC8]/90 justify-center space-x-1 lg:space-x-6 px-0">
           <button
             onClick={toggleMenu}
-            className="hamburger rounded-xl border-2 bg-white px-1.5 pb-1 text-center hover:text-black border-rose-200 hover:border-rose-300 hover:shadow-md hover:shadow-rose-100 transition ease-in duration:400 hover:ease-in"
+            className="hamburger rounded-xl border-2 bg-white px-1.5 pb-1 text-center hover:text-black border-rose-200 hover:border-rose-400 hover:shadow-md hover:shadow-rose-100 transition ease-in duration:400 hover:ease-in"
           >
             ✕
           </button>
           <div className="-mt-1">
             <a
-              className="rounded-xl lg:rounded-2xl border-2 border-white bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/80 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
+              className="rounded-xl lg:rounded-2xl border-2 border-[#2181ff]/50 bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/100 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
               href="#"
             >
               Intro
@@ -38,7 +38,7 @@ const Navbar = () => {
           </div>
           <div className="-mt-1">
             <a
-              className="rounded-xl lg:rounded-2xl border-2 border-white bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/80 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
+              className="rounded-xl lg:rounded-2xl border-2 border-[#2181ff]/50 bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/100 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
               href="#skills"
             >
               Skills
@@ -46,7 +46,7 @@ const Navbar = () => {
           </div>
           <div className="-mt-1">
             <a
-              className="rounded-xl lg:rounded-2xl border-2 border-white bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/80 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
+              className="rounded-xl lg:rounded-2xl border-2 border-[#2181ff]/50 bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/100 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
               href="#experience"
             >
               Experience
@@ -54,7 +54,7 @@ const Navbar = () => {
           </div>
           <div className="-mt-1">
             <a
-              className="rounded-xl lg:rounded-2xl border-2 border-white bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/80 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
+              className="rounded-xl lg:rounded-2xl border-2 border-[#2181ff]/50 bg-white px-1 lg:px-3 mx-1 lg:mx-2 pb-1 mb-2 text-lg lg:text-2xl hover:text-black shadow-md border-[#2181ff]/40 hover:border-[#2181ff]/100 hover:shadow-[#42a2ff]/30 transition ease-in duration:400 hover:ease-in"
               href="#projects"
             >
               Projects
